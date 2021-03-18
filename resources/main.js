1e03d446e078f3643f7cb56a5a7c1da6516b7b6b
Simona Stanciu <simona.stanciu92@gmail.com>
2021-03-18 22:02:19 +0200

    v1

---
+++ resources/main.js
@@ -0,0 +1,83 @@
+$(function() {
+    //MAIN PAGE CONTENT//
+    const productsContentWrapper = $('.product-container');
+    getproductsHTML = function(productObject) {
+        return `<div class="product-item" data-id=${productObject.id}>
+                    <div class="main-image" style="background-image: url(assets/coats/${productObject.imgUrl})"></div>
+                    <div class="main-details">
+                        <div class="product-type">${productObject.name}</div>
+                        <div class="price">${productObject.currency}${productObject.price}</div>
+                    </div>
+                </div>
+        `;
+    };
+
+    for(let x = 0; x < products.coats.length; x++ ) {
+        let productObject = products.coats[x],
+        productHTML = getproductsHTML(productObject);
+        productsContentWrapper.append(productHTML);
+    };
+
+    //OVERLAY CONTENT//
+    const productItemOverlay = $('.product-item-overlay');
+    const overlay = $('.overlay');
+    getproductDetailsHTML = function(productDetails) {
+        return `<div class="product-item-overlay" data-id=${productDetails.id}>
+                    <div class="product-image" style="background-image=url(assets/coats/${productDetails.imgUrl})"></div>
+                    <div class="product-details">
+                        <div class="product-title">${productDetails.name}</div>
+                        <div class="product-price">${productDetails.currency}${productDetails.price}</div>
+                        <div class="product-size">
+                            <div>Your Size</div>
+                            <span>S</span>
+                            <span>M</span>
+                            <span>L</span>
+                            <span>XL</span>
+                        </div>  
+                        <div class="menu-links">
+                            <a href="">Details</a>
+                            <a href="">Order</a>
+                            <a href="">Payment</a>
+                        </div>
+                        <div class="product-item-details">
+                            <div class="first-row"> 
+                                <div>
+                                    <div>Composition</div>
+                                    <div>${productDetails.composition}</div>
+                                </div>
+                                <div>
+                                    <div>Country</div>
+                                    <div>${productDetails.country}</div>
+                                </div>
+                            </div>    
+                            <div class="second-row">
+                                <div>Care</div>
+                                <div>${productDetails.care}</div>
+                            </div>
+                        </div>
+                        <button class="add-button" type="submit">Add to Cart</button>
+                    </div>
+                </div>
+        `;
+    };
+
+    for(let y = 0; y < products.coats.length; y++ ) {
+        let productDetails = products.coats[y],
+        productDetailsHTML = getproductDetailsHTML(productDetails);
+        overlay.append(productDetailsHTML);
+    };
+
+    //OPEN/CLOSE OVERLAY//
+    const productItem = $('.product-item');
+    const overlayWrapper = $('.overlay-wrapper');
+    const overlayContainer = $('.overlay-container');
+    const closeBtn = $('.closeBtn');
+
+    productItem.click(function(){
+        overlayContainer.removeClass('hidden');
+    });
+
+    closeBtn.click (function(){
+        overlayContainer.addClass('hidden');
+    });
+});
---
+++ resources/products.js
@@ -0,0 +1,44 @@
+let products = {
+    coats: [
+        {
+            id: 1,
+            name: "Top Coat",
+            currency: "$",
+            price: "105",
+            imgUrl: "coat-1.jpg",
+            composition: "100% polyester",
+            country: "Belgium",
+            care: "A majority wool blend coats can't be washed safely at home in a machine and need to be taken to a specialized cleaner."
+        },
+        {
+            id: 2,
+            name: "Winter Coat",
+            currency: "$",
+            price: "90",
+            imgUrl: "coat-2.jpg",
+            composition: "100% polyester",
+            country: "Italy",
+            care: "Soft and confortable fabric. Don't wash at high temperature and don't iron."
+        },
+        {
+            id: 3,
+            name: "Woolen Coat",
+            currency: "$",
+            price: "120",
+            imgUrl: "coat-3.jpg",
+            composition: "100% polyester",
+            country: "Germany",
+            care: "Suitable for both classic and casual style. Consider cleaning to professional cleaners only."
+        },
+        {
+            id: 4,
+            name: "Blazer Coat",
+            currency: "$",
+            price: "60",
+            imgUrl: "coat-4.jpg",
+            composition: "100% polyester",
+            country: "Poland",
+            care: "Follow the care label on your coat when you cleaning and treat any stains promptly by blotting."
+        }
+    ]
+}
