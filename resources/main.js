cabcadb096e76463aa85121813d4958fcb1bc16a
Roxana Popescu <roxanateodorapopescu@yahoo.com>
2021-04-26 12:51:40 +0300

    Added functionality for menu-item and improved dynamic content on main-image click

--- resources/main.js
+++ resources/main.js
@@ -4,9 +4,10 @@ $(function() {
     const overlayContainer = $('.overlay-container');
     const closeBtn = $('.closeBtn');
 
-    getproductsHTML = function(index, productObject) {
+    getproductsHTML = function(index, productObject, shopItems) {
+        // la background-image pe randul 10 nu vrei sa iei toate imaginile in coats, ci bazate pe categoria de produs
         return `<div class="product-item" data-id=${productObject.id}>
-                    <div class="main-image" data-index=${index} data-img=${productObject.imgUrl} style="background-image: url(assets/coats/${productObject.imgUrl})"></div>
+                    <div class="main-image" data-index=${index} data-img=${productObject.imgUrl} style="background-image: url(assets/${shopItems}/${productObject.imgUrl})"></div>
                     <div class="main-details">
                         <div class="product-type">${productObject.name}</div>
                         <div class="price">${productObject.currency}${productObject.price}</div>
@@ -16,9 +17,11 @@ $(function() {
     };
 
     function addProducts(shopItems) {
+        // ai nevoie de functia html() aici pentru ca vei adauga produce de o categorie diferita in acest wrapper si trebuie sa "resetezi" inainte
+        productsContentWrapper.html('');
         for(let x = 0; x < products[shopItems].length; x++ ) {
             let productObject = products[shopItems][x],
-                productHTML = getproductsHTML(x, productObject);
+                productHTML = getproductsHTML(x, productObject, shopItems);
                 productsContentWrapper.append(productHTML);
         }
     }
@@ -27,8 +30,12 @@ $(function() {
 
     productsContentWrapper.delegate('.main-image', "click", function() {
         let itemIndex = $(this).data('index');
-        let productDetails = products.coats[itemIndex];
-        $('.product-image').css({backgroundImage: "url(assets/coats/" + $(this).data('img') + ")"});
+        const currentProductCategory = productsContentWrapper.data('selected-products');
+        console.log('currentProductCategory', currentProductCategory)
+        // atentie, aici daca le iei pe toate in functie de coats nu ai sa poti vizualiza dinamic continutul respectiv
+        // let productDetails = products.coats[itemIndex];
+        let productDetails = products[currentProductCategory][itemIndex];
+        $('.product-image').css({backgroundImage: `url(assets/${currentProductCategory}/${$(this).data('img')}`});
         $('.product-title').text(productDetails.name);
         $('.product-price').text(productDetails.currency + productDetails.price);
         $('.composition-type').text(productDetails.composition);
@@ -43,8 +50,13 @@ $(function() {
 
     menuItem.click(function(e) {
         (e).preventDefault();
+        const productCategory = $(this).data('product');
+        console.log('productCategory ', productCategory)
 
         menuItem.removeClass('selected-item');
         $(this).addClass('selected-item');
+
+        addProducts(productCategory);
+        productsContentWrapper.data('selected-products', productCategory);
     });
 });
